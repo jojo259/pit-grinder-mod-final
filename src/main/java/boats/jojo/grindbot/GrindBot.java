dbb88a16e4655cd5fa60e2b1312f294aa2ba35d8
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-02-10 20:26:48 +0000

    incremented version to v1.10

--- src/main/java/boats/jojo/grindbot/GrindBot.java
+++ src/main/java/boats/jojo/grindbot/GrindBot.java
@@ -45,7 +45,7 @@ import net.minecraftforge.fml.common.gameevent.InputEvent;
 @Mod(
 		modid = "keystrokesmod",
 		name = "gb",
-		version = "1.9",
+		version = "1.10",
 		acceptedMinecraftVersions = "1.8.9"
 )
 public class GrindBot

66f23cb70df37d5e52ec3297b94824188e4e3393
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-04-01 06:43:31 +0100

    increment version to v1.13

--- src/main/java/boats/jojo/grindbot/GrindBot.java
+++ src/main/java/boats/jojo/grindbot/GrindBot.java
@@ -54,7 +54,7 @@ import net.minecraftforge.fml.common.gameevent.InputEvent;
 @Mod(
 		modid = "keystrokesmod",
 		name = "gb",
-		version = "1.12",
+		version = "1.13",
 		acceptedMinecraftVersions = "1.8.9"
 )
 public class GrindBot

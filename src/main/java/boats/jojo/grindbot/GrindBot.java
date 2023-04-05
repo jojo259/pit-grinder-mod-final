69282f92e124af7200bed61ab4cf2f528d7f3ff7
GoldenStack <goldenfire64yt@gmail.com>
2023-04-04 21:21:54 -0500

    revamped mouse movement

--- src/main/java/boats/jojo/grindbot/GrindBot.java
+++ src/main/java/boats/jojo/grindbot/GrindBot.java
@@ -152,7 +152,10 @@ public class GrindBot
 	long preApiProcessingTime = 0;
 	
 	String apiMessage = "null";
-	
+
+	double mouseVelX, mouseVelY;
+	long lastMouseUpdate;
+
 	@SubscribeEvent
 	public void onKeyPress(InputEvent.KeyInputEvent event) {
 		long curTime = System.currentTimeMillis();
@@ -193,10 +196,12 @@ public class GrindBot
 			if (event.type == ElementType.ARMOR) {
 				return;
 			}
-			
+
+			interpolateMousePosition();
+
 			int screenWidth = event.resolution.getScaledWidth();
 			int screenHeight = event.resolution.getScaledHeight();
-			
+
 			String[][] infoToDraw = {
 				{"Username", mcInstance.thePlayer.getName()},
 				{"FPS", Integer.toString((int) curFps)},
@@ -207,48 +212,48 @@ public class GrindBot
 				{"Z", Double.toString(Math.round(mcInstance.thePlayer.posZ * 10.0) / 10.0)},
 				{"API msg", apiMessage},
 			};
-			
+
 			for(int i = 0; i < infoToDraw.length; i++) {
 				String[] curInfo = infoToDraw[i];
-				
+
 				drawText(curInfo[0] + ": " + curInfo[1], 4, 4 + i * 10, 0xFFFFFF);
 			}
-			
+
 			int drawKeyboardPositionX = screenWidth - 77;
 			int drawKeyboardPositionY = screenHeight - 60;
-			
+
 			if(mcInstance.gameSettings.keyBindForward.isKeyDown()) { // W
 				drawText("W", drawKeyboardPositionX + 41, drawKeyboardPositionY + 4, 0xFFFFFF);
 			}
-			
+
 			if(mcInstance.gameSettings.keyBindBack.isKeyDown()) { // S
 				drawText("S", drawKeyboardPositionX + 41, drawKeyboardPositionY + 22, 0xFFFFFF);
 			}
-			
+
 			if(mcInstance.gameSettings.keyBindLeft.isKeyDown()) { // A
 				drawText("A", drawKeyboardPositionX + 23, drawKeyboardPositionY + 22, 0xFFFFFF);
 			}
-			
+
 			if(mcInstance.gameSettings.keyBindRight.isKeyDown()) { // D
 				drawText("D", drawKeyboardPositionX + 59, drawKeyboardPositionY + 22, 0xFFFFFF);
 			}
-			
+
 			if(mcInstance.gameSettings.keyBindSneak.isKeyDown()) { // Shift
 				drawText("Sh", drawKeyboardPositionX + 2, drawKeyboardPositionY + 22, 0xFFFFFF);
 			}
-			
+
 			if(mcInstance.gameSettings.keyBindSprint.isKeyDown()) { // Ctrl
 				drawText("Ct", drawKeyboardPositionX + 3, drawKeyboardPositionY + 40, 0xFFFFFF);
 			}
-			
+
 			if(mcInstance.gameSettings.keyBindJump.isKeyDown()) { // Space
 				drawText("Space", drawKeyboardPositionX + 28, drawKeyboardPositionY + 40, 0xFFFFFF);
 			}
-			
+
 			if(mcInstance.gameSettings.keyBindAttack.isKeyDown() || attackedThisTick) { // Mouse1
 				drawText("LM", drawKeyboardPositionX + 2, drawKeyboardPositionY + 4, 0xFFFFFF);
 			}
-			
+
 			if(mcInstance.gameSettings.keyBindUseItem.isKeyDown()) { // Mouse2
 				drawText("RM", drawKeyboardPositionX + 20, drawKeyboardPositionY + 4, 0xFFFFFF);
 			}
@@ -278,13 +283,13 @@ public class GrindBot
 		catch(Exception e){
 			e.printStackTrace();
 		}
-			
+
 		// doing bot tick
-		
+
 		lastTickTime = curTime;
-		
+
 		attackedThisTick = false;
-		
+
 		if (grinderEnabled) {
 			mcInstance.gameSettings.fovSetting = fovWhenGrinding;
 			doBotTick();
@@ -960,6 +965,8 @@ public class GrindBot
 	}
 
 	public void goAfk() {
+		mouseVelX = 0;
+		mouseVelY = 0;
 		allKeysUp();
 		pressChatKeyIfNoGuiOpen();
 	}
@@ -1020,69 +1027,83 @@ public class GrindBot
 	public void drawText(String text, float x, float y, int col) {
 		mcInstance.fontRendererObj.drawStringWithShadow(text, x, y, 0xffffff);
 	}
-	
+
+	public void interpolateMousePosition() {
+		if (!grinderEnabled || mcInstance.currentScreen != null) {
+			lastMouseUpdate = 0;
+			mouseVelX = 0;
+			mouseVelY = 0;
+			return;
+		}
+
+		long currentTime = System.currentTimeMillis();
+		if (lastMouseUpdate != 0) {
+			float timePassed = (currentTime - lastMouseUpdate) / 1000f;
+
+			// Limit time passed (so that the change can't be that large)
+			timePassed = Math.min(timePassed, 1);
+
+			mcInstance.thePlayer.rotationYaw += mouseVelY * timePassed;
+			mcInstance.thePlayer.rotationPitch += mouseVelX * timePassed;
+
+			// Limit pitch
+			float pitch = mcInstance.thePlayer.rotationPitch;
+			mcInstance.thePlayer.rotationPitch = Math.min(Math.max(-90, pitch), 90);
+		}
+		lastMouseUpdate = currentTime;
+	}
+
 	public void mouseMove() {
+		interpolateMousePosition();
+
+		float currentYaw = mcInstance.thePlayer.rotationYaw, currentPitch = mcInstance.thePlayer.rotationPitch;
+		double x = mcInstance.thePlayer.posX, y = mcInstance.thePlayer.posY, z = mcInstance.thePlayer.posZ;
+
+		double headHeight = 1.62;
+
 		// old af math probably stupid
-		double targetRotY = fixRotY(360 - Math.toDegrees(Math.atan2(mouseTargetX - mcInstance.thePlayer.posX, mouseTargetZ - mcInstance.thePlayer.posZ)));
-		double flatDist = Math.sqrt((mouseTargetX - mcInstance.thePlayer.posX) * (mouseTargetX - mcInstance.thePlayer.posX) + (mouseTargetZ - mcInstance.thePlayer.posZ) * (mouseTargetZ - mcInstance.thePlayer.posZ));
-		double targetRotX = -Math.toDegrees(Math.atan((mouseTargetY - mcInstance.thePlayer.posY - 1.62) / flatDist));
+		double targetRotY = fixRotY(360 - Math.toDegrees(Math.atan2(mouseTargetX - x, mouseTargetZ - z)));
+		double targetRotX = -Math.toDegrees(Math.atan(
+				(mouseTargetY - y - headHeight) / Math.hypot(mouseTargetX - x, mouseTargetZ - z)
+		));
 		
 		// add random waviness to target
-		
-		targetRotY += timeSinWave(310) * 2;
-		targetRotY += timeSinWave(500) * 2;
-		targetRotY += timeSinWave(260) * 2;
-		
-		targetRotX += timeSinWave(290) * 2;
-		targetRotX += timeSinWave(490) * 2;
-		targetRotX += timeSinWave(270) * 2;
+		targetRotY += timeSinWave(310) * 2 + timeSinWave(500) * 2 + timeSinWave(260) * 2;
+		targetRotX += timeSinWave(290) * 2 + timeSinWave(490) * 2 + timeSinWave(270) * 2;
 		
 		targetRotY = fixRotY(targetRotY);
 		targetRotX = fixRotX(targetRotX);
 		
 		// calculate mouse speed
+		double timeNoise = timeSinWave(40)  * 2 +
+				timeSinWave(50)  * 2 +
+				timeSinWave(100) * 2 +
+				timeSinWave(150) * 4 +
+				timeSinWave(200) * 6;
+
+		double mouseCurSpeed = mouseSpeed + timeNoise;
+
+		currentYaw = (float) fixRotY(currentYaw);
 		
-		double mouseCurSpeed = mouseSpeed;
+		double diffRotX = targetRotX - currentPitch;
+		double diffRotY = range180(targetRotY - currentYaw);
 		
-		mouseCurSpeed += timeSinWave (40)  * 2;
-		mouseCurSpeed += timeSinWave (50)  * 2;
-		mouseCurSpeed += timeSinWave (100) * 2;
-		mouseCurSpeed += timeSinWave (150) * 4;
-		mouseCurSpeed += timeSinWave (200) * 6;
+		double rotAng = Math.atan2(diffRotY, diffRotX) + Math.PI;
 		
-		mcInstance.thePlayer.rotationYaw = (float) fixRotY(mcInstance.thePlayer.rotationYaw);
-		
-		double diffRotX = targetRotX - mcInstance.thePlayer.rotationPitch;
-		double diffRotY = targetRotY - fixRotY(mcInstance.thePlayer.rotationYaw);
-		
-		if(diffRotY > 180) {
-			diffRotY -= 360;
-		}
-		else if(diffRotY < -180) {
-			diffRotY += 360;
-		}
-		
-		double rotAng = Math.toDegrees(Math.atan2(diffRotY, diffRotX)) + 180;
-		
-		double changeRotX = -Math.cos(Math.toRadians(rotAng)) * mouseCurSpeed / 4;
-		double changeRotY = -Math.sin(Math.toRadians(rotAng)) * mouseCurSpeed;
-		
-		if (true) {
-			if(Math.abs(diffRotX) < Math.abs(changeRotX)) {
-				mcInstance.thePlayer.rotationPitch = (float) targetRotX;
-			}
-			else {
-				mcInstance.thePlayer.rotationPitch += changeRotX;
-			}
-			
-			if(Math.abs(diffRotY) < Math.abs(changeRotY)) {
-				changeRotY = targetRotY - mcInstance.thePlayer.rotationYaw;
-				mcInstance.thePlayer.rotationYaw = (float) targetRotY;
-			}
-			else {
-				mcInstance.thePlayer.rotationYaw += changeRotY;
-			}
-		}
+		double changeRotX = -Math.cos(rotAng) * mouseCurSpeed / 4;
+		double changeRotY = -Math.sin(rotAng) * mouseCurSpeed;
+
+		mouseVelX = Math.abs(diffRotX) < Math.abs(changeRotX)
+				? targetRotX - currentPitch
+				: changeRotX;
+		mouseVelY = Math.abs(diffRotY) < Math.abs(changeRotY)
+				? targetRotY - currentYaw
+				: changeRotY;
+
+		// Reach target in 1/20th of a second (1 tick)
+		double TPS = 20.0;
+		mouseVelX *= TPS;
+		mouseVelY *= TPS;
 	}
 	
 	public boolean farFromMid() {
@@ -1098,7 +1119,15 @@ public class GrindBot
 		num = Math.toRadians(num);
 		num = Math.sin(num);
 		return num;
-	  }
+	}
+
+	public double range180(double rot) {
+		rot = rot % 360;
+		if (rot > 180) {
+			rot -= 360;
+		}
+		return rot;
+	}
 	
 	public double fixRotY(double rotY) {
 		rotY = rotY % 360;
